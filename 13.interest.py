b09c24e1e2cd10e78e968d7121bc150c4eb5ded5
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-06 23:38:27 +0100

    feat: refactor input validation into reusable function for principle, rate, and time

--- 13.interest.py
+++ 13.interest.py
@@ -4,32 +4,24 @@ principle =0
 rate = 0
 time = 0
 
-while principle <= 0:
-    try:
-        principle = float(input("Enter the principle amount: "))
-    except ValueError:
-        print("Please enter a valid number")
+def get_input(inputMessage:str, errorMessage:str):
+    while True:
+        try:
+            userInput = float(input(inputMessage))
+            if userInput <= 0:
+                print(errorMessage)
+            else:
+                return userInput
+        except ValueError:
+            print("Please enter a valid number")
 
-    if(principle <= 0):
-        print("Principle amount must be greater than 0")
 
-while rate <= 0:
-    try:
-        rate = float(input("Enter the rate of interest: "))
-    except ValueError:
-        print("Please enter a valid number")
 
-    if(rate <= 0):
-        print("Rate of interest must be greater than 0")
-  
-while time <= 0:
-    try:
-        time = float(input("Enter the time in years: "))
-    except ValueError:
-        print("Please enter a valid number")
+principle = get_input("Enter the principle amount: ", "Principle amount must be greater than 0")
 
-    if(time <= 0):
-        print("Time must be greater than 0")
+rate = get_input("Enter the rate of interest: ", "Rate of interest must be greater than 0")
+
+time = get_input("Enter the time in years: ", "Time must be greater than 0")
     
 compound_interest = principle * (pow((1 + rate / 100), time))
 
