ec91d044aa8cd50ab5ec87feb79ae3d66d58d911
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-06 16:35:18 +0100

    feat: add formatted price display examples with different alignment and separators

---
+++ 12.formats.py
@@ -0,0 +1,12 @@
+
+
+
+price1 = 3.14159
+price2 = -987.42
+price3 = 12000000.25
+
+
+print(f"Price 1: {price1:_>20} €")
+print(f"Price 2: {price2:_^20} €")
+print(f"Price 3: {price3:_<20} €")
+print(f"Price 3: {price3:,} €")
