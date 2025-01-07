747f7a0dc27ecf26a674eedb441cce6bb103963b
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-07 23:30:33 +0100

    feat: add number guessing game and dictionary of capitals

---
+++ 1.teoria/18.dict.py
@@ -0,0 +1,22 @@
+
+
+
+capitals = {
+    "Argentina": "Buenos Aires",
+    "Brasil": "Brasilia",
+    "Chile": "Santiago",
+    "Uruguay": "Montevideo"
+}
+
+keys=capitals.keys()
+values=capitals.values()
+
+# print(keys, values)
+ 
+# for k,v in capitals.items():
+#     print(k,v)
+
+# argentinaCapital=capitals.get("Argentina") == capitals["Argentina"]
+
+# capitals["Argentina"]="Cordoba" == capitals.update({"Argentina":"Cordoba"})
+
---
+++ 2.practica/5.num_guess/num_guess.py
@@ -0,0 +1,10 @@
+import random
+
+low = 1
+high = 6
+
+number = random.randint(low, high)
+ 
+print("Welcome to the number guessing game!")
+print("I'm thinking of a number between 1 and 6")
+
